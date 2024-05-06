812e8e7499a0402cfa3b7ce19205025c5dfc910e
QORSAN <139044396+SINSQQ@users.noreply.github.com>
2024-05-06 22:13:31 +0200

    Update core.py

--- driver/core.py
+++ driver/core.py
@@ -20,7 +20,9 @@ user = Client(
 
 calls = PyTgCalls(user)
 
-me_user = user.get_me()
 
 with Client(":veez:", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
     me_bot = app.get_me()
+    
+with user as app:
+    me_user = app.get_me()
