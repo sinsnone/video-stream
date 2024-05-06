b63feb2eae4dc69645c6f5ad11918fbd7cd63700
QORSAN <139044396+SINSQQ@users.noreply.github.com>
2024-05-06 23:19:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import asyncio
+import logging
 
 from program import LOGS
 from pytgcalls import idle
 from driver.core import calls, bot, user
 
 
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
 async def start_bot():
     await bot.start()
-    LOGS.info("[ INFO ] BOT & USERBOT CLIENT STARTED")
-    await calls.start()
-    LOGS.info("[ INFO ] PY-TGCALLS CLIENT STARTED")
-    await user.join_chat("iwiwwiq")
-    await user.join_chat("xxStitch")
+    try:
+        await user.start()
+        await calls.start()
+        await user.join_chat("iwiwwiq")
+        await user.join_chat("xxStitch")
+    except Exception as e:
+        logger.exception(e)
+    print("تم تشغيل البوت بنجاح")
     await idle()
-    LOGS.info("[ INFO ] BOT & USERBOT CLIENT STOPPED")
-    await bot.stop()
+    await Bot.stop()
+    print("تم ايقاف البوت بنجاح")
 
 loop = asyncio.get_event_loop_policy().get_event_loop()
 loop.run_until_complete(start_bot())
