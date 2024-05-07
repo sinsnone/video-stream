3704d8974a4ab07755aa81e56fc2d5baa6ac93bd
QORSAN <139044396+SINSQQ@users.noreply.github.com>
2024-05-07 17:12:31 +0200

    ...

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import logging
 from pyrogram import Client, idle
-from pytgcalls import PyTgCalls
-from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
-
+from config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URL, SESSION_NAME, BOT_USERNAME, DEV, LOGGER_ID, OWNER_NAME, appp, user as usr, helper as ass, call
+from pyromod import listen
+from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from plugins.info import activecall, helper, active
+from plugins.Data import db, dev, devname, set_must, get_data
 
 bot = Client(
     "MainBot",
@@ -22,14 +25,40 @@ user = Client(
 # Configure the logger
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Create a logger instance
+logger = logging.getLogger(__name__)
+mongodb = _mongo_client_(MONGO_DB_URL)
+mo = MongoClient()
+mo = MongoClient(MONGO_DB_URL)
+moo = mo["data"]
+Bots = moo.njsa
+db = mongodb.db
+botdb = db.botdb
+blockdb = db.blocked
+Done = []
+OFF = True
 
 async def start_bot():
+    data = {"bot_username": BOT_USERNAME, "token": BOT_TOKEN, "session": SESSION_NAME, "dev": DEV, "logger": LOGGER_ID, "logger_mode": "ON"}
+    Bots.insert_one(data)
+    activecall[BOT_USERNAME] = []
+    dev[BOT_USERNAME] = DEV
     await bot.start()
+    try:
+        devo = await bot.get_chat(DEV)
+        devo = devo.first_name
+        devname[BOT_USERNAME] = devo
+    except:
+        devname[BOT_USERNAME] = OWNER_NAME
     try:
         await user.start()
     except Exception as e:
         logger.exception(e)
+    appp[BOT_USERNAME] = bot
+    usr[BOT_USERNAME] = user
+    ass[BOT_USERNAME] = []
+    await helper(BOT_USERNAME)
     print("تم تشغيل البوت بنجاح")
     await idle()
-    await bot.stop()
+    await Bot.stop()
     print("تم ايقاف البوت بنجاح")
